f14f4a16a804255b4b89f317415095d324db9381
Victor Osses <v254373@dac.unicamp.br>
2022-01-21 23:47:11 -0300

    Corrigindo exibição de mensagens de update

--- CRUD-Mysqli/php_action/update.php
+++ CRUD-Mysqli/php_action/update.php
@@ -14,7 +14,21 @@ include_once 'db_connect.php';
         include_once 'sanitize-validate.php';
 
         if(isset($_SESSION['mensagem'])):
-            header('Location: ../editar.php');
+            header('Location: ../editar.php?id='.$_POST['id']);
+        else:
+            $sql = "UPDATE clientes SET nome = '$nome', sobrenome='$sobrenome', email='$email', idade='$idade' WHERE id=".$_POST['id'];
+            $resultado = mysqli_query($connect, $sql);
+
+            if($resultado):
+                $_SESSION['mensagem'] = "Cliente atualizado com sucesso!";
+                mysqli_close($connect);
+                header('Location: ../index.php');
+            else:
+                $_SESSION['mensagem'] = "Falha ao tentar atualizar cliente!";
+                mysqli_close($connect);
+                header('Location: ../index.php');
+            endif;
+
         endif;
     else:
         header('Location: ../index.php');
